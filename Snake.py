e4e50fcba9f63399243e58a79098f1d2a4f4b8e0
BrandonBowden1996 <45600694+brandonbowden@users.noreply.github.com>
2020-05-31 17:27:02 +0100

    Setting up the screen

--- Snake.py
+++ Snake.py
@@ -1,1 +1,26 @@
 #Snake Game
+import turtle
+import os
+
+#set up the screen
+screen = turtle.Screen()
+screen.bgcolor("black")
+screen.title("Snake")
+
+#Drawing the game border
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300, -300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range(4):
+	border_pen.fd(600)
+	border_pen.lt(90)
+border_pen.hideturtle()
+
+
+
+
+delay = input("Press enter to finish")

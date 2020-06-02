e2425b2b1c88dc35a1b78ff87dacb6e51c1e8465
BrandonBowden1996 <45600694+brandonbowden@users.noreply.github.com>
2020-06-02 22:26:19 +0100

    Adding the player snake turtle and adding key binding

--- Snake.py
+++ Snake.py
@@ -20,7 +20,51 @@ for side in range(4):
 	border_pen.lt(90)
 border_pen.hideturtle()
 
+#Create the player turtle
+Player = turtle.Turtle()
+Player.color("White")
+Player.shape("square")
+Player.penup()
+Player.speed(0)
+Player.setposition(0,0)
 
+playerspeed = 15
+
+#Move the player up, down, left and right
+def move_up():
+	y = Player.ycor()
+	y += playerspeed
+	if y > 280:
+		y = 280
+	Player.sety(y)
+	
+def move_down():
+	y = Player.ycor()
+	y -= playerspeed
+	if y < -280:
+		y = -280
+	Player.sety(y)
+	
+def move_left():
+	x = Player.xcor()
+	x -= playerspeed
+	if x < -280:
+		x = -280
+	Player.setx(x)
+	
+def move_right():
+	x = Player.xcor()
+	x += playerspeed
+	if x > 280:
+		x = 280
+	Player.setx(x)
+	
+#Creat the keyboard binding
+turtle.listen()
+turtle.onkey(move_up, "Up")
+turtle.onkey(move_down, "Down")
+turtle.onkey(move_left, "Left")
+turtle.onkey(move_right, "Right")
 
 
 delay = input("Press enter to finish")
